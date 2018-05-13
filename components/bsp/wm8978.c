846e4dc86ae837c3b1a0b140ace2daa59fa0bfb8
frankie <frankie.zengfu@gmail.com>
2018-05-14 07:55:44 +0800

    wm8978 driver

--- components/bsp/wm8978.c
+++ components/bsp/wm8978.c
@@ -1,11 +1,9 @@
 #include "bsp.h"
 
-#include "wm8978.h"
-#include "hal_i2c.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "hal_i2s.h"
+
 
 
 #define bit0  0x001
@@ -17,7 +15,8 @@
 #define bit6  0x040 
 #define bit7  0x080 
 #define bit8  0x100
- 	
+
+#define WM8978_ADDR	 0X1A
 
 static uint16_t WM8978_REGVAL_TBL[58]=
 {
@@ -31,21 +30,71 @@ static uint16_t WM8978_REGVAL_TBL[58]=
 	0X0001,0X0001
 }; 
 
+#define SET_BIT(x,y) (x|=y)
+#define CLEAR_BIT(x,y) (x&=~y)
+#define CONFIG_BIT(x,y,z) (if(z){SET_BIT(x,y);}else{CLEAR_BIT(x,y);})
 
 
-
-uint8_t WM8978_Write_Reg(uint8_t reg,uint16_t val)
+static uint8_t wm8978_write_reg(uint8_t index)
 { 
 	uint8_t buf[2];
-	buf[0]=((val&0x0100)>>8)|(reg<<1);
+	val=WM8978_REGVAL_TBL[index];
+	buf[0]=((val&0x0100)>>8)|(index<<1);
 	buf[1]=(uint8_t)(val&0xff);
 	hal_i2c_master_mem_write(0,WM8978_ADDR,buf[0],buf+1,1);
-	WM8978_REGVAL_TBL[reg]=val;
 	return 0;	
 }  
 
-uint16_t WM8978_Read_Reg(uint8_t reg)
+static uint16_t wm8978_read_reg(uint8_t reg)
 {  
 	return WM8978_REGVAL_TBL[reg];	
 } 
+
+typedef struct{
+	uint16_t L_PGA_EN:1;
+	uint16_t R_PGA_EN:1;
+	uint16_t LIP_PGA:1;
+	uint16_t LIN_PGA:1;
+	uitn16_t RIP_PGA:1;
+	uint16_t RIN_PGA:1;
+	uint16_t R2_PGA:1;
+	uint16_t L2_PGA:1;
+    uint16_t ALC:2;
+	uint16_t L_BOOST_EN:1;
+	uint16_t R_BOOST_EN:1;
+
+}InputCfgDef;
+
+void wm8978_pga_vol(uin8_t left,uint8_t right){
+	WM8978_REGVAL_TBL[45]&=~(0x3f);
+	WM8978_REGVAL_TBL[45]|=left;
+
+	WM8978_REGVAL_TBL[46]&=~(0x3f);
+	WM8978_REGVAL_TBL[46]|=right;
+	wm8978_write_reg(45);
+	wm8978_write_reg(46);
+}
+void wm8978_pag_mute(uint8_t left,uint8_t right){
+	
+}
+
+void wm8978_input_cfg(){
+	InputPathCfgDef input_cfg;
+	//pga enable
+	CONFIG_BIT(WM8978_REGVAL_TBL[0x02],bit2,input_cfg->L_PGA_EN);
+	CONFIG_BIT(WM8978_REGVAL_TBL[0x02],bit3,input_cfg->R_PGA_EN);
+	//input control
+	CONFIG_BIT(WM8978_REGVAL_TBL[44],bit0,input_cfg->LIP_PGA);
+	CONFIG_BIT(WM8978_REGVAL_TBL[44],bit1,input_cfg->LIN_PGA);
+	CONFIG_BIT(WM8978_REGVAL_TBL[44],bit2,input_cfg->L2_PGA);
+
+	CONFIG_BIT(WM8978_REGVAL_TBL[44],bit4,input_cfg->RIP_PGA);
+	CONFIG_BIT(WM8978_REGVAL_TBL[44],bit5,input_cfg->RIN_PGA);
+	CONFIG_BIT(WM8978_REGVAL_TBL[44],bit6,input_cfg->R2_PGA);
+	//alc control
+	WM8978_REGVAL_TBL[32]&=~(0x3<<7);
+	WM8978_REGVAL_TBL[32]|=(input_cfg->ALC<<7);
+}
+
+
 
